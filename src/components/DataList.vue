08fb9ce1fa898c48813a04a9751fbb425361365c
Samir Massoundi <samir.massoundi@ynov.com>
2021-03-21 20:39:58 +0100

    Pagination done

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -1,20 +1,14 @@
 <template>
-  <!-- <button v-on:click='importJson'>GET DATA</button>
-  <p>{{ peopleList }}</p> -->
   <section class="section">
-    <p>{{ $store.state._peopleList.length}} Resultats</p>
+    <p>{{ $store.state.filteredPeopleList.length }} Resultats</p>
 
-    <!-- <p>{{$store.state._peopleList}}</p> -->
+    <recherche @champsrecherche="filteredList" />
 
-    <recherche @champsrecherche="filteredList" v-model="arraySearch"/>
-
-    
-    <button @click="exportJSON">Exporter JSON</button>
     <div class="container is-fluid table-container">
       <table class="table is-striped is-fullwidth is-hoverable">
         <thead>
           <tr>
-            <!-- <th>id</th> -->
+            <th>id</th>
             <th>Prenom</th>
             <th>Nom</th>
             <th>Genre</th>
@@ -31,8 +25,8 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="person in filteredList" v-bind:key="person.id">
-            <!-- <td>{{ person.id }}</td> -->
+          <tr v-for="person in pagination" v-bind:key="person.id">
+            <td>{{ person.id }}</td>
             <td>{{ person.firstname }}</td>
             <td>{{ person.lastname }}</td>
             <td>{{ person.gender }}</td>
@@ -59,98 +53,63 @@
         role="navigation"
         aria-label="pagination"
       >
-        <a class="pagination-previous">Previous</a>
-        <a class="pagination-next">Next page</a>
+        <a class="pagination-previous" @click="previousPage">Précédent</a>
+
         <ul class="pagination-list">
-          <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
-          <li><span class="pagination-ellipsis">&hellip;</span></li>
-          <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
           <li>
-            <a
-              class="pagination-link is-current"
-              aria-label="Page 46"
-              aria-current="page"
-              >46</a
-            >
-          </li>
-          <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
-          <li><span class="pagination-ellipsis">&hellip;</span></li>
-          <li>
-            <a
-              class="pagination-link"
-              aria-label="Goto page `{{(peopleList.length)/10}}`"
-              >{{ peopleList.length / 10 }}</a
-            >
+            <a class="pagination-link is-current">{{ currentPage }}</a>
           </li>
         </ul>
+        <a class="pagination-next" @click="nextPage">Suivant</a>
       </nav>
     </div>
   </section>
 </template>
 
 <script>
-const fs = require("fs");
+// const fs = require('fs');
+
+import { mapGetters, mapState } from 'vuex';
 import recherche from './recherche.vue';
 export default {
   name: 'dataList',
   components: {
     recherche,
-  }, 
+  },
   data() {
     return {
-      peopleList: [],
       arraySearch: '',
+      personPerPage: '10',
+      currentPage: '1',
     };
   },
-  props:
-  {
-
-  },
+  props: {},
   methods: {
-    // filtrer(searchedWord, searchedField) {
-    //   console.log('we received the emit');
-    //   console.log(searchedWord);
-    //   console.log(searchedField);
-
-    //   let tempPeopleList = this.peopleList;
-
-    //   if (searchedWord != '' && searchedWord) {
-    //     tempPeopleList = tempPeopleList.filter((person) => {
-    //       return person.firstname
-    //         .toUpperCase()
-    //         .includes(searchedWord.toUpperCase());
-    //     });
-    //   }
-    //   return tempPeopleList;
-    // },
-    exportJson(){
-        let data = JSON.stringify(this.peopleList);
-        fs.writeFileSync("people.json", data);
-    }
-  },
-  mounted() {
-    var requestURL =
-      'https://run.mocky.io/v3/70e5b0ad-7112-41c5-853e-b382a39e65b7';
-    fetch(requestURL)
-      .then((response) => response.json())
-      .then((data) => (this.peopleList = data.people));
-  },
-  computed: {
-    filteredList() {
-        console.log("we receive");
-      let tempPeopleList = this.peopleList;
-        let i=2
-      if (i==2) {
-        tempPeopleList = tempPeopleList.filter((person) => {
-          return person.lastname.includes('');
-        });
+    nextPage() {
+      if (
+        this.currentPage * this.personPerPage <
+        this.$store.state.peopleList.length
+      ) {
+        this.currentPage++;
       }
-      return tempPeopleList;
     },
-
-
+    previousPage() {
+      if (this.currentPage > 1) this.currentPage--;
+    },
+  },
+  mounted() {},
+  computed: {
+    filteredList(message) {
+      console.log(message);
+      return 0;
+    },
+    ...mapGetters(['getPersonById', 'getPersonCount']),
+    ...mapState(['peopleList', 'filteredPeopleList']),
+    pagination()
+    {
+        return this.$store.state.peopleList.slice((this.currentPage - 1) * this.personPerPage, this.currentPage * this.personPerPage )
+    }
   },
 };
 </script>
-
 <style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,23 +2,33 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    _peopleList: [], 
+    peopleList: [],
+    filteredPeopleList:[], 
+
  },
   mutations: {
-      SET_PEOPLE_LIST: ( state, _peopleList) =>
+      SET_PEOPLE_LIST: ( state, peopleList) =>
       {
-          state._peopleList = _peopleList;
+          state.peopleList = peopleList;
+      },
+      SET_FILTERED_PEOPLE_LIST: ( state, filteredPeopleList) =>
+      {
+          state.filteredPeopleList = filteredPeopleList;
+      },
+      RESET_FILTERED: (state) => {
+        state.filteredPeopleList = state.peopleList
       }
   },
   getters:
   {
     getPersonById: (state) => (id) =>
     {
-        return state._peopleList.find(_peopleList =>_peopleList.id === id );
+        return state.peopleList.find(peopleList =>peopleList.id === id );
     },
     getPersonCount: (state) => {
-        return state._peopleList.length
+        return state.peopleList.length
     }
+    
   },
   actions: {
     peopleListToStore (context) {
@@ -27,23 +37,10 @@ export default createStore({
         .then((response) => response.json())
         .then((data) => {
             context.commit("SET_PEOPLE_LIST", data.people);
+            context.commit("SET_FILTERED_PEOPLE_LIST", data.people);
         });
   },
 },
   modules: {
   }
 })
-
-
-// const setPeopleListToStore = () =>{
-//     let requestURL = 'https://run.mocky.io/v3/70e5b0ad-7112-41c5-853e-b382a39e65b7';
-//   fetch(requestURL)
-//     .then((response) => response.json())
-//     .then((data) => {
-//         $store.commit("SET_PEOPLE_LIST", data.people);
-//     });
-
-
-// }
-
-// setPeopleListToStore();
